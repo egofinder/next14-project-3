b329b3b4f7290ade2f6ee99477d0d7db1640f806
Benjamin Jeon <baekhakjeon@gmail.com>
2024-01-17 16:47:05 -0800

    Add dependencies and update styles

---
+++ src/components/custom-ui/mobile-sidebar.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { Menu } from "lucide-react";
+import { useEffect, useState } from "react";
+
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet";
+import Sidebar from "./sidevar";
+
+const MobileSidebar = () => {
+  // TODO: Hydration 에러를 고치는 트릭인데 좀더 자세히 이해해보기
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileSidebar;
